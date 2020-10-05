dae710d9f370c735d9c406101a9558459a982e5b
kiborgok <borgokalek14@gmail.com>
2020-10-05 13:34:19 +0300

    Commented out redux-immutable-state-invariant devDep

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -4,16 +4,17 @@ import {
     compose
 } from 'redux';
 import rootReducer from './reducers/rootReducer';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+//import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 
 const configureStore = initialState => {
     //add support to redux-dev-tools
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    //const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     return createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+        applyMiddleware(thunk)
+        //composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
     );
 }
 
